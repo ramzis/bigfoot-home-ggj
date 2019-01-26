9d6471b428aacfb4827926038f039a26dd22f19e
Anthony Ha <haa4@uci.edu>
2019-01-25 23:26:54 -0800

    Spawn points can be placed with SpawnContainer

---
+++ bigfoot-home-ggj/Assets/Scripts/SpawnEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof(SpawnContainer))]
+public class SpawnEditor : Editor {
+
+    private static bool EditorEnabled = false;
+    private GameObject SpawnPrefab;
+    private GameObject SpawnParent;
+
+    void OnSceneGUI()
+    {
+        if (EditorEnabled)
+        {
+            if (Event.current.type == EventType.MouseDown)
+            {
+                Ray r = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                RaycastHit hitInfo;
+                if (Physics.Raycast(r, out hitInfo))
+                {
+                    SpawnParent = GameObject.Find("Spawns");
+                    SpawnPrefab = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Spawn.prefab", typeof(GameObject));
+                    GameObject newPoint = GameObject.Instantiate(SpawnPrefab);
+                    newPoint.transform.position = hitInfo.point + new Vector3 (0f, 0.5f, 0f);
+                    newPoint.transform.parent = SpawnParent.transform;
+                }
+            }
+        }
+    }
+	
+    public override void OnInspectorGUI(){
+        EditorEnabled = GUILayout.Toggle(EditorEnabled, "Enable Editor");
+    }
+}
