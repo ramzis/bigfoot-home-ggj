58d558e4bb4580eb6269564ba93e27954dc07076
Anthony Ha <haa4@uci.edu>
2019-01-26 15:17:56 -0800

    SpawnEditor fills a list in SpawnContainer when adding points

--- bigfoot-home-ggj/Assets/Scripts/SpawnContainer.cs
+++ bigfoot-home-ggj/Assets/Scripts/SpawnContainer.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnContainer : MonoBehaviour {
-    
+    public List<GameObject> SpawnList = new List<GameObject>();
+
+    void Start(){
+        //Debug.Log("Number of spawn points: " + SpawnList.Count);
+    }
 }
