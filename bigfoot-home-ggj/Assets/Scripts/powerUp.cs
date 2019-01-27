522e4248446596aa41b1716902fc0139bbb3bc95
Andy Le <azuredragon013@yahoo.com>
2019-01-26 22:26:24 -0800

    Explosion PowerUp
    There is a prefab for an explosion powerUp object that when collected, will play the explosion vfx and deal damage to houses around it.
    Range and Damage are adjustable in the editor.

---
+++ bigfoot-home-ggj/Assets/Scripts/powerUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerUp : MonoBehaviour {
+    public string power;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        if (this.GetComponent<Explosion>().explosion.isPlaying)
+        {
+            this.GetComponent<Renderer>().enabled = false;
+            this.GetComponent<CapsuleCollider>().enabled = false;
+            if(this.GetComponent<Explosion>().explosion.isStopped)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(power == "explode")
+            {
+                this.GetComponent<Explosion>().explode();
+                //Destroy(this.gameObject);
+            }
+        }
+    }
+}
