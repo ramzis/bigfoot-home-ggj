643ac33195315084f37dda0dfdc01be0e082452f
Anthony Ha <haa4@uci.edu>
2019-01-27 03:11:01 -0800

    Begin player controller implementation

---
+++ bigfoot-home-ggj/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    [SerializeField] float m_MoveSpeed = 7.5f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    bool m_Grounded;
+    CapsuleCollider m_Collider;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
